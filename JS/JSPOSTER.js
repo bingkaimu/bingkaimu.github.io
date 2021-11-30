9ebc4bcf036112a53d93137d394308242531875c
Maskibow <iszroil@gmail.com>
2021-11-30 11:59:52 +0700

    Update JSPOSTER.js

--- JS/JSPOSTER.js
+++ JS/JSPOSTER.js
@@ -303,61 +303,45 @@ document.getElementById("Bingkaix").style.display = "block";
 }
 
 function hideallsubs() {
-document.getElementById("submenu").style.display = "none"; 
-document.getElementById("submenu").style.height = "auto"; 	
-document.getElementById("subtema").style.display = "none"; 
-document.getElementById("subtema").style.height = "auto"; 	
-document.getElementById("sublogo").style.display = "none"; 
-document.getElementById("sublogo").style.height = "auto"; 	
-document.getElementById("subframe").style.display = "none";
-document.getElementById("subframe").style.height = "auto"; 	
-document.getElementById("subfoto").style.display = "none"; 
-document.getElementById("subfoto").style.height = "auto"; 	
-document.getElementById("subtitle").style.display = "none"; 
-document.getElementById("subtitle").style.height = "auto"; 	
-document.getElementById("subnote").style.display = "none"; 
-document.getElementById("subnote").style.height = "auto";	
-document.getElementById("subhelp").style.display = "none"; 
-document.getElementById("subhelp").style.height = "auto";	
+document.getElementById("submenu").style.height = "0px"; 	
+document.getElementById("subtema").style.height = "0px"; 	
+document.getElementById("sublogo").style.height = "0px"; 	
+document.getElementById("subframe").style.height = "0px"; 	
+document.getElementById("subfoto").style.height = "0px"; 	
+document.getElementById("subtitle").style.height = "0px"; 	
+document.getElementById("subnote").style.height = "0px";	
+document.getElementById("subhelp").style.height = "0px";	
 }
 
 function showSubmenu() {
 hideallsubs();
-document.getElementById("submenu").style.display = "block";
 document.getElementById("submenu").style.height = "auto"; 	
 }
 function showSubtema() {
 hideallsubs();
-document.getElementById("subtema").style.display = "block"; 
 document.getElementById("subtema").style.height = "auto"; 	
 }
 function showSublogo() {
 hideallsubs();
-document.getElementById("sublogo").style.display = "block"; 
 document.getElementById("sublogo").style.height = "auto"; 	
 }
 function showSubframe() {
 hideallsubs();
-document.getElementById("subframe").style.display = "block"; 
 document.getElementById("subframe").style.height = "auto"; 
 }
 function showSubfoto() {
 hideallsubs();
-document.getElementById("subfoto").style.display = "block"; 
 document.getElementById("subfoto").style.height = "auto"; 
 }
 function showSubjudul() {
 hideallsubs();
-document.getElementById("subtitle").style.display = "block";
 document.getElementById("subtitle").style.height = "auto"; 	
 }
 function showSubnote() {
 hideallsubs();
-document.getElementById("subnote").style.display = "block";
 document.getElementById("subnote").style.height = "auto"; 	
 }
 function showSubhelp() {
 hideallsubs();
-document.getElementById("subhelp").style.display = "block"; 
 document.getElementById("subhelp").style.height = "auto"; 	
 }
