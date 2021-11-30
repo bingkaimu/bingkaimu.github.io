5016ef7e21fa9bed2b843d544b0219fc18dc3811
Maskibow <iszroil@gmail.com>
2021-11-30 15:48:29 +0700

    Update JSPOSTER.js

--- JS/JSPOSTER.js
+++ JS/JSPOSTER.js
@@ -311,7 +311,6 @@ document.getElementById("subfoto").style.height = "0px";
 document.getElementById("subtitle").style.height = "0px"; 	
 document.getElementById("subnote").style.height = "0px";	
 document.getElementById("subsave").style.height = "0px";
-document.getElementById("submenu").style.display = "none"; 	
 document.getElementById("subtema").style.display = "none"; 	
 document.getElementById("subframe").style.display = "none"; 	
 document.getElementById("subfoto").style.display = "none"; 	
@@ -322,44 +321,36 @@ document.getElementById("subsave").style.display = "none";
 
 function showSubmenu() {
 hideallsubs();
-document.getElementById("submenu").style.display = "block"; 	
 document.getElementById("submenu").style.width = "300px";
-setTimeout(hideallsubs, 22000);	
 }
 function showSubtema() {
 hideallsubs();
 document.getElementById("subtema").style.display = "block";	
 document.getElementById("subtema").style.height = "auto"; 
-setTimeout(hideallsubs, 22000);		
 }
 
 function showSubframe() {
 hideallsubs();
 document.getElementById("subframe").style.display = "block";	
 document.getElementById("subframe").style.height = "auto"; 
-setTimeout(hideallsubs, 22000);		
 }
 function showSubfoto() {
 hideallsubs();
 document.getElementById("subfoto").style.display = "block";	
 document.getElementById("subfoto").style.height = "auto";
-setTimeout(hideallsubs, 22000);		
 }
 function showSubjudul() {
 hideallsubs();
 document.getElementById("subtitle").style.display = "block";	
 document.getElementById("subtitle").style.height = "auto"; 
-setTimeout(hideallsubs, 22000);		
 }
 function showSubnote() {
 hideallsubs();
 document.getElementById("subnote").style.display = "block";	
 document.getElementById("subnote").style.height = "auto"; 
-setTimeout(hideallsubs, 22000);		
 }
 function showSubsave() {
 hideallsubs();
 document.getElementById("subsave").style.display = "block";	
 document.getElementById("subsave").style.height = "auto"; 
-setTimeout(hideallsubs, 22000);		
 }
